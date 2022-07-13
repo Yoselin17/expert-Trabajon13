44c4c66a0e6ce08a21288dc2413be1939d94a30f
yoselin retamal <yoselin_yar@hotmail.com>
2022-07-13 17:09:08 -0400

    code13

---
+++ src/Componentes/Home.js
@@ -0,0 +1,16 @@
+import Menu from './Menu';
+import Card from './Card';
+import Footer from './Footer';
+
+
+function Home() {
+    return (
+        <div>
+        <Menu/>
+        <Card/>
+        <Footer/>
+        </div>
+    );
+}
+
+export default Home;
